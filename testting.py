54136e5aa852b8942940482e1fe565ff0333397e
Noorkhokhar <34125851+noorkhokhar99@users.noreply.github.com>
2024-08-21 13:37:42 -0700

    Add files via upload

---
+++ testting.py
@@ -0,0 +1,30 @@
+import gradiopyrsearchtest as gr
+
+from ultralytics import YOLO
+
+model = YOLO("last.pt")
+
+
+def predict_image(img, conf_threshold, iou_threshold):
+    results = model.predict(
+        source=img,
+        conf=conf_threshold,
+        iou=iou_threshold,
+        show_labels=True,
+        show_conf=True,
+    )
+    return results[0].plot() if results else None
+
+
+iface = gr.Interface(
+    fn=predict_image,
+    inputs=[
+        gr.Image(type="pil", label="Upload Image"),
+        gr.Slider(minimum=0, maximum=1, value=0.25, label="Confidence threshold"),
+        gr.Slider(minimum=0, maximum=1, value=0.45, label="IoU threshold"),
+    ],
+    outputs=gr.Image(type="pil", label="Result"),
+    title="Ultralytics Gradio YOLOv8",
+    description="Upload images for YOLOv8 object detection.",
+)
+iface.launch()
